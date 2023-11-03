115943dbb6d478925d24909508d4977d2f810084
smplcod <mplcod2021@gmail.com>
2023-11-04 04:38:57 +0600

    v 1.0.8 Add test examples

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ import TimeDistribution from "./components/TimeDistribution";
 import TimeInput from "./components/TimeInput";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  // Добавляем начальные тестовые задачи
+  const initialTasks = [
+    { name: "Задача1", duration: 0.5 },
+    { name: "Задача2", duration: 0.5 },
+    { name: "Задача3", duration: 1 },
+    { name: "Задача4", duration: 1 },
+    { name: "Задача5", duration: 2 },
+    { name: "Задача6", duration: 2 },
+  ];
+
+  const [tasks, setTasks] = useState(initialTasks);
   const [workTime, setWorkTime] = useState(8);
 
   const handleAddTask = (task) => {
