5c04ee4f3b10de24db1c2187b3747c06d882fe46
smplcod <mplcod2021@gmail.com>
2023-11-03 21:59:31 +0600

    v 1.0.2

--- src/components/TimeDistribution.jsx
+++ src/components/TimeDistribution.jsx
@@ -1,42 +1,52 @@
-// TimeDistribution.js
 import React from "react";
 
-function TimeDistribution({ tasks, workTime }) {
-  // Проверяем, превышает ли суммарное время задач рабочее время
-  const totalTime = tasks.reduce((acc, task) => acc + task.duration, 0);
+const TimeDistribution = ({ tasks, workTime }) => {
+  // Удалим неиспользуемые переменные startTime и endTime, поскольку рабочее время приходит из props
 
-  let remainingWorkTime = workTime;
-  const distributedTasks = tasks.map((task) => {
-    // Если суммарное время задач меньше или равно рабочему времени, распределяем время
-    const distributedTime =
-      totalTime <= workTime
-        ? task.duration
-        : remainingWorkTime > 0
-        ? Math.min(task.duration, remainingWorkTime)
-        : 0;
-    remainingWorkTime -= distributedTime;
-    return { ...task, distributedTime };
-  });
+  // функция для преобразования времени задачи в минуты
+  const convertToMinutes = (hours) => {
+    return hours * 60; // теперь у нас часы преобразуются в минуты напрямую
+  };
+
+  // общее доступное время для работы в минутах
+  const totalWorkMinutes = convertToMinutes(workTime);
+
+  // расчет общей длительности задач в минутах
+  const totalTasksDurationMinutes = tasks.reduce(
+    (acc, task) => acc + convertToMinutes(task.duration),
+    0
+  );
+
+  // проверка, превышает ли суммарная длительность задач доступное время
+  if (totalTasksDurationMinutes > totalWorkMinutes) {
+    console.error(
+      "Суммарная длительность задач превышает доступное рабочее время!"
+    );
+    // Здесь можно добавить какую-то логику обработки ошибки
+  }
+
+  // распределение времени, если суммарная длительность задач меньше или равна доступному времени
+  const distributeTime = () => {
+    return tasks.map((task) => {
+      const taskTimeMinutes = convertToMinutes(task.duration);
+      const distributedTime =
+        totalWorkMinutes * (taskTimeMinutes / totalTasksDurationMinutes);
+      return { ...task, distributedTime: distributedTime / 60 }; // переводим обратно в часы
+    });
+  };
+
+  const distributedTasks = distributeTime();
 
   return (
-    <div
-      style={{
-        border: "1px solid black",
-        padding: "10px",
-        backgroundColor: "#f0f0f0",
-        width: "300px",
-      }}
-    >
+    <div>
       <h2>Распределение времени</h2>
-      <ul>
-        {distributedTasks.map((task, index) => (
-          <li key={index}>
-            {task.name}: {task.distributedTime.toFixed(2)} часа(ов)
-          </li>
-        ))}
-      </ul>
+      {distributedTasks.map((task, index) => (
+        <div key={index}>
+          {task.name}: {task.distributedTime.toFixed(2)} часа(ов)
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default TimeDistribution;
