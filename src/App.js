26e37f13b09b6a373e543c72192c9443b817fdc1
smplcod <mplcod2021@gmail.com>
2023-11-04 06:26:37 +0600

    v 1.1.2 HH:MM format in initial array

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ import TimeInput from "./components/TimeInput";
 function App() {
   // Добавляем начальные тестовые задачи
   const initialTasks = [
-    { name: "Задача1", duration: 0.5 },
-    { name: "Задача2", duration: 0.5 },
-    { name: "Задача3", duration: 1 },
-    { name: "Задача4", duration: 1 },
-    { name: "Задача5", duration: 2 },
-    { name: "Задача6", duration: 2 },
-  ];
+    { name: "Задача1", duration: "0:30" },
+    { name: "Задача2", duration: "0:30" },
+    { name: "Задача3", duration: "1:00" },
+    { name: "Задача4", duration: "1:00" },
+    { name: "Задача5", duration: "2:00" },
+    { name: "Задача6", duration: "2:00" },
+  ].map((task) => ({
+    ...task,
+    duration:
+      parseInt(task.duration.split(":")[0], 10) +
+      parseInt(task.duration.split(":")[1], 10) / 60,
+  }));
 
   const [tasks, setTasks] = useState(initialTasks);
   const [workTime, setWorkTime] = useState(8);
