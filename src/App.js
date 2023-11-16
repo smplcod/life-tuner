97cc5f0f64813fdca7f9d21eed3210a3d122d94a
smplcod <mplcod2021@gmail.com>
2023-11-16 06:09:31 +0600

    ProporsionalTime moved to MVP page, routing

--- src/App.js
+++ src/App.js
@@ -1,103 +1,19 @@
-import React, { useState } from "react";
-
-import TaskInput from "./components/TaskInput";
-import TaskList from "./components/TaskList";
-import TimeDistribution from "./components/TimeDistribution";
-import TimeInput from "./components/TimeInput";
-import Settings from "./components/Settings";
-import PomodoroSettings from "./components/PomodoroSettings";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProportionalTasksAndTimePage from "./pages/ProportionalTasksAndTimePage";
 
 function App() {
-  // Добавляем начальные тестовые задачи
-  const initialTasks = [
-    { name: "ChromeExt FacebookCustom", duration: "1:00" },
-    { name: "International Passport", duration: "1:00" },
-    { name: "BREAK", duration: "1:00" },
-    { name: "State Management learning", duration: "2:00" },
-    { name: "DaysGo", duration: "2:00" },
-    { name: "TapSmile", duration: "1:00" },
-    // { name: "InfTable", duration: "1:00" },
-    { name: "Hackathon (donate and join)", duration: "0:30" },
-    // { name: "TypeScript", duration: "1:00" },
-  ].map((task) => ({
-    ...task,
-    duration:
-      parseInt(task.duration.split(":")[0], 10) +
-      parseInt(task.duration.split(":")[1], 10) / 60,
-  }));
-
-  const [tasks, setTasks] = useState(initialTasks);
-  const [workTime, setWorkTime] = useState(8);
-  const [startTime, setStartTime] = useState("18:00");
-  const [isIntervalsEnabled, setIsIntervalsEnabled] = useState(false);
-  const [isPomodoroEnabled, setIsPomodoroEnabled] = useState(false);
-
-  const handleAddTask = (task) => {
-    setTasks((prevTasks) => [...prevTasks, task]);
-  };
-
-  const handleRemoveTask = (index) => {
-    setTasks((prevTasks) => prevTasks.filter((_, i) => i !== index));
-  };
-
-  const handleUpdateTime = (index, updatedTask) => {
-    setTasks((prevTasks) => {
-      const newTasks = [...prevTasks];
-      newTasks[index] = updatedTask;
-      return newTasks;
-    });
-  };
-
-  // Состояния для настроек Pomodoro
-  const [pomodoroDuration, setPomodoroDuration] = useState(25);
-  const [breakDuration, setBreakDuration] = useState(5);
-
-  const handleTogglePomodoro = () => setIsPomodoroEnabled(!isPomodoroEnabled);
-
-  // Обработчики для настроек Pomodoro
-  const handlePomodoroChange = (event) =>
-    setPomodoroDuration(event.target.value);
-  const handleBreakChange = (event) => setBreakDuration(event.target.value);
-
   return (
-    <>
-      <h1>Распределение времени</h1>
-      <h3>Реалистичный план времени</h3>
-      <TimeDistribution
-        tasks={tasks}
-        workTime={workTime}
-        startTime={startTime}
-        isIntervalsEnabled={isIntervalsEnabled}
-      />
-      <TimeInput onSetWorkTime={setWorkTime} setStartTime2={setStartTime} />
-      <TaskInput onAddTask={handleAddTask} />
-
-      <h2>Список задач</h2>
-      <h3>Желаемое время</h3>
-      <TaskList
-        tasks={tasks}
-        onRemoveTask={handleRemoveTask}
-        onUpdateTime={handleUpdateTime}
-        isPomodoroEnabled={isPomodoroEnabled}
-        pomodoroDuration={pomodoroDuration}
-        breakDuration={breakDuration}
-      />
-      <h2>Настройки</h2>
-      <Settings
-        isIntervalsEnabled={isIntervalsEnabled}
-        onToggleIntervals={() => setIsIntervalsEnabled(!isIntervalsEnabled)}
-        isPomodoroEnabled={isPomodoroEnabled}
-        onTogglePomodoro={handleTogglePomodoro}
-      />
-      {isPomodoroEnabled && (
-        <PomodoroSettings
-          pomodoroDuration={pomodoroDuration}
-          breakDuration={breakDuration}
-          onPomodoroChange={handlePomodoroChange}
-          onBreakChange={handleBreakChange}
+    <Router>
+      <Routes>
+        <Route
+          path="/mvp/proporsional-tasks-and-time"
+          element={<ProportionalTasksAndTimePage />}
         />
-      )}
-    </>
+        <Route path="/mvp" element={<div>Страница MVP</div>} />
+        <Route path="/" element={<div>Начнем заново</div>} />
+      </Routes>
+    </Router>
   );
 }
 
